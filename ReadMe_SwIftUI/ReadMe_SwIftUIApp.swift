462f9ba5afd9e03af49cea1f372bb9fa6d907276
Bruno Correa <bruno.correa@sicoob.com.br>
2021-12-06 15:04:03 -0300

    Initial Commit

---
+++ ReadMe_SwIftUI/ReadMe_SwIftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMe_SwIftUIApp.swift
+//  ReadMe_SwIftUI
+//
+//  Created by Bruno Teixeira Lemgruber Correa on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMe_SwIftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
